d1fbe6461ced19fceb875e22550eaf6d8d5fccb4
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2023-12-22 18:20:58 +0600

    find data based on id

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.put('/upTasks/:id', async (req, res) => {
     
     
 
-    // Send a ping to confirm a successful connection
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
